330a9025f242b63fa5941ca77ce57795a9c4c7a0
Timo Lietmeyer <timolietmeyer@googlemail.com>
2023-04-17 15:43:13 +0200

    init

---
+++ Code/DCE.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def calc_k(s1,s2)
+    k = (calc_betha(s1,s2)*calc_length(s1)*calc_length(s2))/(calc_length(s1)+calc_length(s2))
+    return k
+
+
+def calc_betha(s1,s2):
+    foo = 0
+    return foo
+
+def calc_length(ls):
+    return ls
