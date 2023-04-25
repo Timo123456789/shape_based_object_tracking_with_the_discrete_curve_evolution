0dac5a6aaed90d1208e8b769e3c3d5a38f7554ad
Timo Lietmeyer <timolietmeyer@googlemail.com>
2023-04-25 19:16:47 +0200

    add main method

--- Code/DCE.py
+++ Code/DCE.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+
+def testdurchlauf():
+    testpolygon = [[0,0],[2,0],[2,2],[3,4],[1,2]]
+    #Alternativ Import einer Textdatei mit Punkten (gleiches Schema wie oben?)
+    return 0        
+
+
+
 def calc_k(s1,s2):
     k = (calc_betha(s1,s2)*calc_length(s1)*calc_length(s2))/(calc_length(s1)+calc_length(s2))
     return k
@@ -11,3 +19,6 @@ def calc_betha(s1,s2):
 
 def calc_length(ls):
     return ls
+
+
+testdurchlauf()
