137714f9b1db978f06274b168f245ecbc80c2225
Timo Lietmeyer <timolietmeyer@googlemail.com>
2023-07-12 11:33:14 +0200

    first version of main file description

--- Code/main.py
+++ Code/main.py
@@ -16,7 +16,6 @@ def main():
     init.py files are important for connecting DCE.py with yolo_every_frame.py and yolo_result_version.py
     """
     options = {
-            #"path_source_video": r'Code\vid_examples\right_Side\autobahn1s.mp4',
             "path_source_video": r'Code\vid_examples\autobahn_long_shot_135s.mp4',
             "path_write_video": r'Code\YOLO\runs\autobahn_ls_135_ev_vers.mp4',
             "path_write_timestamps": r'Code\YOLO\runs\timestamps_autobahn_ls_135_ev_vers.txt',
@@ -55,69 +54,13 @@ def main():
             "number_of_polygons":0,
             "number_of_angles":0
     }
-    test = 0
-    #get_sum_of_angles(test)
-
+  
     if (options["yolo_every_frame"] == True): #if clause to run selected YOLO Version
         run_yolo_every_frame_version(options)
     else:
         run_yolo_result_version(options)
    
 
-def run_test(options):
-    shape_similarity_val = 0
-    img_1 = get_specific_frame(options["path_source_video"],11)
-    img_2 = get_specific_frame(options["path_source_video"],15)
-    img_1_yolo = run_yolo(img_1, options, 11)
-    img_2_yolo = run_yolo(img_2, options, 15)
-    #img_1 = cv2.resize(img_1, fx = 0.5, fy = 0.5)
-    #img_2 = cv2.resize(img_2, fx = 0.5, fy = 0.5)
-    #print(options["list_of_all_polygons"])
-    polygon_array = options["list_of_all_polygons"]
-    print(polygon_array)
-    print(polygon_array[0][0][2])
-    print(polygon_array[1][0][2])
-    print("  ")
-    print(polygon_array[0][0])
-    print(polygon_array[1][0])
-    
-    temp = 0
-  
-    for frame in range(len(polygon_array)-1):
-        print(len(polygon_array[0]))
-        print(polygon_array[0])
-        for polygon in range(len(polygon_array[0])):
-            temp = temp + (polygon_array[frame][polygon][2]-polygon_array[frame+1][polygon][2])
-    print("temp")            
-    print(temp)            
-    cv2.imshow("image", img_1)       
-    cv2.waitKey(0) 
-    # cv2.imshow("image", img_2)       
-    # cv2.waitKey(0) 
-
-    
-    # cv2.imshow("image", img_1_yolo)       
-    # cv2.waitKey(0) 
-    # print(options["angle_sums_polygons"])
-    # print(len(options["angle_sums_polygons"]))
-  
-    # print(len(options["angle_sums_polygons"]))
-    print(options["angle_sums_images"])
-    print(len(options["angle_sums_images"]))
-
-
-
-    # for i in range(len(options["angle_sums_images"])-1):
-    #     shape_similarity_val +=  options["angle_sums_images"][i] - options["angle_sums_images"][i+1]
-
-    print("shape_similarity_val")
-    print(shape_similarity_val)
-    cv2.imshow("image", img_1_yolo)       
-    cv2.waitKey(0) 
-    cv2.imshow("image", img_2_yolo)       
-    cv2.waitKey(0) 
-
-    return 0
 
 
 def run_yolo_every_frame_version(options):
@@ -164,12 +107,6 @@ def run_yolo_result_version(options):
     if(options["save_timestamps"]==True): #if clause to save the timestamps
         save_timestamps_as_file_yolo_result(options)
     return 0
-
-
-
-
-
-
 
 
 
@@ -246,7 +183,65 @@ def ret_timestampline(options, string):
         return str(time_in_ms) +" ms / "+str(time_in_s)+" sec /" + str(time_in_min)+" min," 
     else:
         return str(time_in_ms) +" ms / "+str(time_in_s)+" sec," 
-   
+
+
+
+
+def run_test(options):
+    shape_similarity_val = 0
+    img_1 = get_specific_frame(options["path_source_video"],11)
+    img_2 = get_specific_frame(options["path_source_video"],15)
+    img_1_yolo = run_yolo(img_1, options, 11)
+    img_2_yolo = run_yolo(img_2, options, 15)
+    #img_1 = cv2.resize(img_1, fx = 0.5, fy = 0.5)
+    #img_2 = cv2.resize(img_2, fx = 0.5, fy = 0.5)
+    #print(options["list_of_all_polygons"])
+    polygon_array = options["list_of_all_polygons"]
+    print(polygon_array)
+    print(polygon_array[0][0][2])
+    print(polygon_array[1][0][2])
+    print("  ")
+    print(polygon_array[0][0])
+    print(polygon_array[1][0])
+    
+    temp = 0
+  
+    for frame in range(len(polygon_array)-1):
+        print(len(polygon_array[0]))
+        print(polygon_array[0])
+        for polygon in range(len(polygon_array[0])):
+            temp = temp + (polygon_array[frame][polygon][2]-polygon_array[frame+1][polygon][2])
+    print("temp")            
+    print(temp)            
+    cv2.imshow("image", img_1)       
+    cv2.waitKey(0) 
+    # cv2.imshow("image", img_2)       
+    # cv2.waitKey(0) 
+
+    
+    # cv2.imshow("image", img_1_yolo)       
+    # cv2.waitKey(0) 
+    # print(options["angle_sums_polygons"])
+    # print(len(options["angle_sums_polygons"]))
+  
+    # print(len(options["angle_sums_polygons"]))
+    print(options["angle_sums_images"])
+    print(len(options["angle_sums_images"]))
+
+
+
+    # for i in range(len(options["angle_sums_images"])-1):
+    #     shape_similarity_val +=  options["angle_sums_images"][i] - options["angle_sums_images"][i+1]
+
+    print("shape_similarity_val")
+    print(shape_similarity_val)
+    cv2.imshow("image", img_1_yolo)       
+    cv2.waitKey(0) 
+    cv2.imshow("image", img_2_yolo)       
+    cv2.waitKey(0) 
+
+    return 0
+
 
 
 
